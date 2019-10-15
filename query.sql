82414eaffd219e14ed30fcdd4ae169645557590f
njokuann <njokuann@gmail.com>
2019-10-14 20:06:48 -0500

    push

--- query.sql
+++ query.sql
@@ -12,22 +12,6 @@ price VARCHAR(255),
 city VARCHAR(255)
 );
 
-DROP table AirbnbWA;
-DROP table AirbnbTX;
-
-CREATE TABLE AirbnbTry (
-bedrooms INT PRIMARY KEY,
-price Varchar(255),
-city VARCHAR(255)
-);
 
 SELECT * FROM airbnbwa;
 SELECT * FROM airbnbtx;
-
-Drop table airbnbwa;
-drop table airbnbtx;
-
-select airbnbwa.bedrooms, airbnbwa.price, airbnbwa.city
-from airbnbwa
-join airbnbtx
-on airbnbtx.id = airbnbwa.id;
