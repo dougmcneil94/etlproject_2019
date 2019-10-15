dc81636db57d9285bb65c46bcffc8e088436c80c
njokuann <njokuann@gmail.com>
2019-10-14 19:59:11 -0500

    push

--- query.sql
+++ query.sql
@@ -1,28 +1,33 @@
 CREATE TABLE airbnbwa (
-Bedrooms VARCHAR(35) PRIMARY KEY,
-Price INT NOT NULL,
-City VARCHAR(255)
+id INT PRIMARY KEY, 
+bedrooms VARCHAR(35),
+price INT NOT NULL,
+city VARCHAR(255)
 );
 
 CREATE TABLE airbnbtx (
-Bedrooms VARCHAR(35) PRIMARY KEY,
-Price INT NOT NULL,
-City VARCHAR(255)
+id INT PRIMARY KEY, 
+bedrooms VARCHAR(35),
+price VARCHAR(255),
+city VARCHAR(255)
 );
 
 DROP table AirbnbWA;
 DROP table AirbnbTX;
 
 CREATE TABLE AirbnbTry (
-Bedrooms INT PRIMARY KEY,
-Price INT NOT NULL,
-City VARCHAR(255)
+bedrooms INT PRIMARY KEY,
+price Varchar(255),
+city VARCHAR(255)
 );
 
 SELECT * FROM airbnbwa;
 SELECT * FROM airbnbtx;
 
-select airbnbwa.bedrooms, airbnb.price, airbnbwa.city
-from Airbnbwa
-left join airbnbtx
-on airbnbtx.bedrooms = airbnbwa.bedrooms;
+Drop table airbnbwa;
+drop table airbnbtx;
+
+select airbnbwa.bedrooms, airbnbwa.price, airbnbwa.city
+from airbnbwa
+join airbnbtx
+on airbnbtx.id = airbnbwa.id;
